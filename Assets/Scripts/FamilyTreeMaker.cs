4535768bdf54d958d1b3a30f9672c88920cc97c8
vtaipale <valtter.taipale@gmail.com>
2023-11-04 20:51:33 +0200

    Version 8
    FamilyTreeMaker draws a rought but functioning family tree!
    Additionally marks dead characters.

--- Assets/Scripts/FamilyTreeMaker.cs
+++ Assets/Scripts/FamilyTreeMaker.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+///Constructs a readable family tree
 public class FamilyTreeMaker : MonoBehaviour
 {
 	
@@ -9,10 +10,12 @@ public class FamilyTreeMaker : MonoBehaviour
 	
 	//public int sideChange = 0;
 	
+	public Mesh DeadMesh;
+	
     // Start is called before the first frame update
     void Start()
     {
-      this.setupkids();  
+      //this.setupkids();  
     }
 
     // Update is called once per frame
@@ -22,11 +25,12 @@ public class FamilyTreeMaker : MonoBehaviour
     }
 	public void makeFamilyTree ()
 	{
-		//sideChange = 0;
 		
-		//this.setupkids();
+		this.setupkids();
 		
-		MoveFamilyMember(FamilyRoot, 0, 0);
+		MoveFamilyMember(FamilyRoot, 0);
+		
+		this.HandleDeadPersons(805);
 		
 	}
 	
@@ -34,9 +38,9 @@ public class FamilyTreeMaker : MonoBehaviour
 	{
 		foreach (FamilyMember Person in FamilyRoot.GetComponentsInChildren<FamilyMember>())
 		{
-			//Person.getImportantParent().MyKids.Add(Person);
+			Person.getImportantParent().MyKids.Add(Person);
 			
-			
+			/*
 			if (Person.father != null)
 			{
 				Person.father.MyKids.Add(Person);
@@ -45,18 +49,36 @@ public class FamilyTreeMaker : MonoBehaviour
 			{
 				Person.mother.MyKids.Add(Person);
 			}
-			
+			*/
 		}
 		
 	}
 	
-	private void MoveFamilyMember(FamilyMember CurrentPerson, int KidNumber, int sideChange)
+	public void HandleDeadPersons(int CurrentYear)
 	{
+		foreach (FamilyMember Person in FamilyRoot.GetComponentsInChildren<FamilyMember>())
+		{
+			if (Person.death < CurrentYear)
+		
+			Person.transform.GetComponent<MeshFilter>().mesh =  DeadMesh;
+	
+		}
+		
+		
+	}
+	
+	private int MoveFamilyMember(FamilyMember CurrentPerson, int KidNumber)
+	{
+		int howmuchMoved = 0;
+		
 		//CurrentPerson.gameObject.transform.position.y = (CurrentPerson.getImportantParent().gameObject.transform.position.y-1);
 		
 		//CurrentPerson.gameObject.transform.position.x = (CurrentPerson.getImportantParent().gameObject.transform.position.x-KidNumber);
 		
-		CurrentPerson.transform.position = CurrentPerson.getImportantParent().transform.position + new Vector3(sideChange,-1,0);
+		CurrentPerson.transform.position = CurrentPerson.getImportantParent().transform.position + new Vector3(KidNumber,-1,0);
+		
+		//CurrentPerson.transform.position = new Vector3(sideChange,-CurrentPerson.Generation,0);
+		
 		
 		//CurrentPerson.transform.localPosition = new Vector3(0,-1, 0);
 		//CurrentPerson.transform.localPosition = CurrentPerson.transform.localPosition + new Vector3(sideChange,0, 0);
@@ -68,11 +90,13 @@ public class FamilyTreeMaker : MonoBehaviour
 			for (int index = 0; index < CurrentPerson.MyKids.Count; index++)
 			{
 				if (index > 0)
-					sideChange++;
+					howmuchMoved++;
 				
-				MoveFamilyMember(CurrentPerson.MyKids[index],index,sideChange);
+				howmuchMoved += MoveFamilyMember(CurrentPerson.MyKids[index],howmuchMoved);
 			}
 		}
+		
+		return howmuchMoved;
 
 	}
 	
